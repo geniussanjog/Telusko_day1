460202b5012d1386309c4c2c94e90667d11eceb7
Genius Sanjog <genius.sanjog@gmail.com>
2023-05-19 21:35:19 +0530

    Initial Commit Day 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int num = 5;
+        int result =  factorial(num);
+        System.out.println(result);
+    }
+
+    private static int factorial(int num) {
+        if(num==1)
+            return 1;
+        return num* factorial(num-1);
+    }
+}
